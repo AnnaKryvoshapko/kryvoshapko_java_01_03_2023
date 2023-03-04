11bfd2671c40594de4371033193940d0ef3c0bf1
Anna Kryvoshapko <anna.kryvoshapko@gmail.com>
2023-03-04 20:35:09 +0200

    HW_3_Coordinates

---
+++ src/HW_3_Coordinates/MyApp.java
@@ -0,0 +1,10 @@
+package HW_3_Coordinates;
+
+public class MyApp {
+    public static void main (String[]args){
+        double Latitude = 48.8582573;
+        double Longitude = 2.2945111;
+            System.out.println("Eiffel Tower");
+            System.out.println("Latitude:" + Latitude + " Longitude:" + Longitude);
+        }
+}
--- src/MyApp.java
+++
@@ -1,8 +0,0 @@
-public class MyApp {
-        static double a = 48.8582573;
-        static double b = 2.2945111;
-        public static void main (String[]args){
-            System.out.println("Eiffel Tower");
-            System.out.println("Latitude:" + a + " Longitude:" + b);
-        }
-}
